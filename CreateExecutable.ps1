7581adc75e09e8677d0bac9226b34ba8edbcc97a
Mathijs Kolkhuis Tanke <mathijs.kolkhuistanke@ortec-finance.com>
2021-05-11 00:35:20 +0200

    Zorg altijd voor een schone build vanuit de installer

--- CreateExecutable.ps1
+++ CreateExecutable.ps1
@@ -1,4 +1,11 @@
 Push-Location $PSScriptRoot
+If (Test-Path -Path ".\dist" -PathType Container) {
+    Remove-Item -Path ".\dist" -Recurse
+}
+If (Test-Path -Path ".\build" -PathType Container) {
+    Remove-Item -Path ".\build" -Recurse
+}
+
 Start-Process -FilePath "pyinstaller.exe" -ArgumentList "--onefile", ".\src\PageGetter.py", "--icon=$PSScriptRoot\src\Spuit.ico" -Wait
 Copy-Item -Path ".\src\Spuit.ico" -Destination ".\dist\"
 Pop-Location
