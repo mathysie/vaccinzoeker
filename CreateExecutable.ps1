2fe26aa3db473e4d7272c92b4b3f499e243f5dd6
Mathijs Kolkhuis Tanke <mathijs.kolkhuistanke@ortec-finance.com>
2021-05-11 00:36:39 +0200

    Hernoem main script

--- CreateExecutable.ps1
+++ CreateExecutable.ps1
@@ -6,6 +6,6 @@ If (Test-Path -Path ".\build" -PathType Container) {
     Remove-Item -Path ".\build" -Recurse
 }
 
-Start-Process -FilePath "pyinstaller.exe" -ArgumentList "--onefile", ".\src\PageGetter.py", "--icon=$PSScriptRoot\src\Spuit.ico" -Wait
+Start-Process -FilePath "pyinstaller.exe" -ArgumentList "--onefile", ".\src\Main.py", "--icon=$PSScriptRoot\src\Spuit.ico" -Wait
 Copy-Item -Path ".\src\Spuit.ico" -Destination ".\dist\"
 Pop-Location
